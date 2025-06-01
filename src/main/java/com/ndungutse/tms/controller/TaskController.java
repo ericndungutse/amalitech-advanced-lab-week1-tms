5f5f7ec2105e2598a4dd879a114af8d0b746c40e
ericndungutse <dav.ndungutse@gmail.com>
2025-06-01 14:07:26 +0200

    feat: add swagger documentation

--- src/main/java/com/ndungutse/tms/controller/TaskController.java
+++ src/main/java/com/ndungutse/tms/controller/TaskController.java
@@ -16,16 +16,22 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 @RestController
 @RequestMapping("/api/v1/tasks")
+@Tag(name = "Task Management", description = "Operations related to task management")
 public class TaskController {
 
     @GetMapping
+    @Operation(summary = "Get All Tasks", description = "Retrieve all tasks in the system")
     public ResponseEntity<List<?>> getAllTasks() {
         return new ResponseEntity<List<?>>(new ArrayList<>(), HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Get task by ID", description = "Retrieve a task by its unique identifier")
     public ResponseEntity<?> getTaskById(@PathVariable("id") String id) {
         Map<String, Object> newTask = new HashMap<>();
         newTask.put("id", "sdfkd-34rewr-43rer453");
@@ -34,6 +40,7 @@ public class TaskController {
     }
 
     @PostMapping
+    @Operation(summary = "Create a new task", description = "Add a new task to the system")
     public ResponseEntity<?> createTask(@RequestBody Map<String, Object> taskRequest) {
         Map<String, Object> newTask = new HashMap<>();
         newTask.put("id", "sdfkd-34rewr-43rer453");
@@ -42,6 +49,7 @@ public class TaskController {
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Update an existing task", description = "Modify an existing task by its unique identifier")
     public ResponseEntity<?> updateTask(@PathVariable("id") String id, @RequestBody Map<String, Object> taskRequest) {
         Map<String, Object> task = new HashMap<>();
         task.put(id, id);
@@ -50,6 +58,7 @@ public class TaskController {
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Delete a task", description = "Remove a task from the system by its unique identifier")
     public ResponseEntity<?> deleteTask(@PathVariable("id") String id) {
         Map<String, Object> response = new HashMap<>();
         response.put("message", "Task deleted successfully");
